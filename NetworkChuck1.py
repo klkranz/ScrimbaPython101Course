dd0060006a08effda43d9d6f745cdbb2ad04d1bb
klkranz <kkranz@protonmail.com>
2022-05-21 19:00:27 -0600

    Added NetworkChuck1.py from Network Chuck's videos on Python to play along

---
+++ NetworkChuck1.py
@@ -0,0 +1,6 @@
+print("Welcome, to NetworkChuck Coffee!!")
+name = input("What is your name?\n")
+menu = "Black Coffey, Espresso, Latte, and Cappuccino"
+print("Today our menu is: " + menu)
+order = input(f"What can I get for you today, {name}?\n")
+print(f"Thank you, {name}. Your {order} will be ready in a few moments.")
